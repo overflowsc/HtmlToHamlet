6b1f04d53dd6555a99a2e47ceeb161d570f0c0fb
Mathieu Parent <mathieuparent5@gmail.com>
2013-11-07 21:29:36 -0500

    v1.0

---
+++ HamletWriter.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HtmlToHamlet
+{
+    public class HamletWriter : XmlTextWriter
+    {
+        private static char[] assetsFolderSeparators = { '\\', '/' };
+        private static string[] excludedTags = { "head", "header", "footer" };
+        private static string[] contentOnlyTags = { "html", "body", "footer" };
+
+        private int indentCount;
+        private string currentElement;
+        private string currentAttribute;
+        private string imageFolderName;
+        private string cssFolderName;
+        private string javascriptFolderName;
+
+        public HamletWriter(string filename, string imageFolderName, string cssFolderName, string javascriptFolderName)
+            : base(filename, Encoding.UTF8)
+        {
+            Indentation = 0;
+            Formatting = Formatting.None;
+            currentElement = "";
+            currentAttribute = "";
+            indentCount = 0;
+            this.imageFolderName = imageFolderName;
+            this.cssFolderName = cssFolderName;
+            this.javascriptFolderName = javascriptFolderName;
+        }
+
+        private string ExtractFileName(string filePath)
+        {
+            string[] srcPath = filePath.Split(assetsFolderSeparators);
+            if (srcPath.Length == 1)
+            {
+                return srcPath.Last();
+            }
+
+            string fileName = srcPath.Last();
+            if (fileName.Length == 0)
+            {
+                fileName = srcPath[srcPath.Length - 2];
+            }
+            return fileName;
+        }
+
+        private string ReplaceNonAlphanumeric(string str)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9]");
+            return rgx.Replace(str, "_");
+        }
+
+        private string BuildImageSrc(string currentSrc)
+        {
+            string fileName = ExtractFileName(currentSrc);
+            fileName = imageFolderName + '_' + fileName;
+            return "@{StaticR " + ReplaceNonAlphanumeric(fileName) + "}";
+        }
+
+        private string BuildCssSrc(string currentSrc)
+        {
+            string fileName = ExtractFileName(currentSrc);
+            fileName = cssFolderName + '_' + fileName;
+            return "@{StaticR " + ReplaceNonAlphanumeric(fileName) + "}";
+        }
+
+        private string BuildJavascriptSrc(string currentSrc)
+        {
+            string fileName = ExtractFileName(currentSrc);
+            fileName = javascriptFolderName + '_' + fileName;
+            return "@{StaticR " + ReplaceNonAlphanumeric(fileName) + "}";
+        }
+
+        public override void WriteProcessingInstruction(string name, string text)
+        {
+            // Do not remove, must not output the xml processing instruction
+        }
+
+        public override void WriteEndElement()
+        {
+            indentCount--;
+            currentElement = "";
+        }
+
+        public override void WriteComment(string text)
+        {
+            // Hack, if the was a doctype declaration, the html parser did not read it correctly and have a comment node with that contant in it.
+            // Just ignore it
+            if (!text.Contains("CTYPE ht"))
+            {
+                base.WriteComment(text);
+            }
+        }
+
+        public override void WriteStartElement(string prefix, string localName, string ns)
+        {
+            currentElement = localName;
+            base.WriteString("\r\n");
+            WriteIndent();
+            base.WriteStartElement(prefix, localName, ns);
+            indentCount++;
+        }
+
+        public override void WriteString(string text)
+        {
+            string formattedText = text;
+            switch (currentAttribute)
+            {
+                case "src":
+                    switch (currentElement)
+                    {
+                        case "img":
+                            formattedText = BuildImageSrc(text);
+                            break;
+                        case "script":
+                            formattedText = BuildJavascriptSrc(text);
+                            break;
+                    }
+                    break;
+                case "href":
+                    switch (currentElement)
+                    {
+                        case "link":
+                            formattedText = BuildCssSrc(text);
+                            break;
+                    }
+                    break;
+            }
+            base.WriteString(formattedText);
+        }
+
+        private void WriteIndent()
+        {
+            for (int i = 0; i < indentCount; i++)
+            {
+                base.WriteString("    ");
+            }
+        }
+
+        public override void WriteStartAttribute(string prefix, string localName, string ns)
+        {
+            currentAttribute = localName;
+            base.WriteStartAttribute(prefix, localName, ns);
+        }
+
+
+        public override void WriteEndAttribute()
+        {
+            currentAttribute = "";
+            base.WriteEndAttribute();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HtmlToHamlet
+{
+    
+    public class Program
+    {
+        private static string outputFolder = "templates";
+        private static string tempFolder = "temp";
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No input files.");
+                Console.WriteLine("Usage: put the path to the files to convert in argument separated with spaces.");
+                Console.ReadLine(); //Pause
+                return;
+            }
+
+
+            try
+            {
+                System.IO.FileInfo tempFolderFile = new System.IO.FileInfo(tempFolder + Path.DirectorySeparatorChar + "whatever");
+                tempFolderFile.Directory.Create();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("## Could not create the temporary folder for conversion. Make sure that you have write access.");
+                Console.Write(e.StackTrace + "\n");
+                Console.ReadLine(); //Pause
+                return;
+            }
+
+            try
+            {
+                System.IO.FileInfo tempFolderFile = new System.IO.FileInfo(outputFolder + Path.DirectorySeparatorChar + "whatever");
+                tempFolderFile.Directory.Create();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("## Error creating the output folder. Make sure that you have write access on your drive.");
+                Console.Write(e.StackTrace + "\n");
+                Console.ReadLine(); //Pause
+                return;
+            }
+
+            foreach (string filename in args)
+            {
+                Console.WriteLine("\n\n" + filename + "---------------------------");
+                string[] tokens = filename.Split('.');
+
+                if (tokens.Length<2)
+                {
+                    Console.WriteLine("## Error for file " + filename + ". The file format could not be found.");
+                    continue;
+                }
+                else
+                {
+                    if (tokens.Last() != "html")
+                    {
+                        Console.WriteLine("## Error for file " + filename + ". The file is not an html file.");
+                        continue;
+                    }
+
+                    string newExtension = "hamlet";
+
+                    string filenameWithoutExtention=tokens[tokens.Length-2];
+                    string outputFileName = outputFolder + Path.DirectorySeparatorChar + filenameWithoutExtention + "." + newExtension;
+                    string tempFilename = tempFolder + Path.DirectorySeparatorChar + filename;
+                    
+
+                    StreamReader sr = new StreamReader(filename, Encoding.UTF8);
+                    StringBuilder sb = new StringBuilder();
+                    
+                    while(!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        line = line.Trim();
+                        sb.AppendLine(line);
+                    }
+
+                    string fullFileContent = sb.ToString();
+                    fullFileContent = fullFileContent.Replace("\r\n", "");
+                    fullFileContent = fullFileContent.Replace("\n", "");
+                    fullFileContent = fullFileContent.Replace("&nbsp;", " #");
+
+                    try
+                    {
+                        StreamWriter sw = new StreamWriter(new FileStream(tempFilename, FileMode.Create), Encoding.UTF8);
+                        sw.Write(fullFileContent);
+                        sw.Flush();
+                        sw.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("## Error creating the temporary file. Make sure that you have write access on your drive.");
+                        Console.Write(e.StackTrace + "\n");
+                        continue;
+                    }
+
+                    HtmlDocument document = new HtmlDocument();
+                    
+                    try 
+                    {
+                        document.Load(tempFilename);
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("## Error parsing the HTML file.");
+                        Console.Write(e.StackTrace + "\n");
+                        continue;
+                    }
+
+
+                    HamletWriter formatter;
+                    try
+                    {
+                        formatter = new HamletWriter(outputFileName, "img", "css", "js");
+                    }
+                    catch (UnauthorizedAccessException e)
+                    {
+                        Console.WriteLine("## Error writing the hamlet file. Make sure you have access on your drive.");
+                        Console.Write(e.StackTrace + "\n");
+                        continue;
+                    }
+                    catch (DirectoryNotFoundException e)
+                    {
+                        Console.WriteLine("## Error writing the hamlet file. Make sure you have access on your drive and that the \"template\" folder exists.");
+                        Console.Write(e.StackTrace + "\n");
+                        continue;
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("## Error writing the hamlet file. Make sure you have access on your drive.");
+                        Console.Write(e.StackTrace + "\n");
+                        continue;
+                    }
+                    catch (System.Security.SecurityException e)
+                    {
+                        Console.WriteLine("## Error writing the hamlet file. You do not have access to this drive.");
+                        Console.Write(e.StackTrace + "\n");
+                        continue;
+                    }
+
+                    try 
+                    {
+                        document.DocumentNode.WriteTo(formatter);
+                        formatter.Flush();
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("## Error creating the the Hamlet file");
+                        Console.Write(e.StackTrace + "\n");
+                        continue;
+                    }
+                    Console.WriteLine(filename + " conversion successful.");
+                }
+            }
+            Console.WriteLine("\n\nConversion complete. The converted files are located in the \"templates\" folder.");
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine(); //Pause
+        }
+    }
+}
